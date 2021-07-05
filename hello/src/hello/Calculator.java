3b2ac7499f222eaa2d419dffde25be5096b706c1
nikhilpm009 <nikhilpm009@gmail.com>
2021-07-05 08:23:22 +0530

    diffrence funcion added to calculator

--- hello/src/hello/Calculator.java
+++ hello/src/hello/Calculator.java
@@ -1,12 +1,21 @@
 package hello;
 
 public class Calculator {
-	 
 
-	int sum (int x, int y){
+	int sum(int x, int y) {
 
-return (x+y);
+		return (x + y);
 	}
+	
+	
+	
+  byte diff(byte a, byte b){
+	  
+	  byte result=0;
+	  result=(byte) (a-b);
+	  return result;
+  }
+  
 }
 
 // TODO Auto-generated method stub
--- hello/src/hello/maths.java
+++ hello/src/hello/maths.java
@@ -5,11 +5,17 @@ public class maths {
 	public static void main(String[] args) {
 		int num = 250;
 		System.out.println("this is num:" + num);
-		num = num * 2;
+		num  = num * 2;
 		System.out.println("the value of num * 2 is");
-		System.out.println(" == " + num + " is the out put");
+		System.out.println(" == " + num + " is the output");
 		Calculator cacio = new Calculator();
 		int sum = cacio.sum(9, 7);
 		System.out.println("the sum is " + sum);
+	byte diff ;
+	byte a=9;
+	byte b=2;
+	diff = cacio.diff(a, b);
+	System.out.println("the diff is " + diff);
+	
 	}
 }
